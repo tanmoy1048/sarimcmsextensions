5aa46b0943ec190e2c3603a96fdd6d8a98bc1897
Sarim2005 <Sarim2005@gmail.com>
2011-06-23 08:53:41 +0000

    replaced while loop with for loop

--- dec2bin.c
+++ dec2bin.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
-
+#include<math.h>
 int main() {
-int dec,a,v,k=0;
+int dec,a,v,k=0,lt;
 int bin[80];
 printf("Enter the decimal number : ");
 scanf("%d",&dec);
 a = dec;
-do {
+lt = log ( dec ) / log (2);
+for (k = 0 ; k <= lt ; k++){
 v = a % 2;
 bin[k] = v;
 a = a / 2 ;
-k++; } while (a > 0);
+}
 printf("\nThe binary value of %d is ",dec);
 for (k;k!=0;k=k-1) printf("%d",bin[k-1]);
 printf("\n\nThank You\n");
